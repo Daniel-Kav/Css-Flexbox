751a9ca7a97f8cad127ab4ca364f5e38312857a2
daniel-kav <dankavatha01@gmail.com>
2024-05-11 13:52:42 +0300

    styled the html

--- script.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector('.menu-bar');
-const navMenu = document.querySelector('.navbar');
-
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('active');
-  navMenu.classList.toggle('active');
-});
